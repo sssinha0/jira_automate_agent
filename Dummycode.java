833a61453079e9ddb9114d56bfb282527de431c6
sssinha0 <b180043@nitsikkim.ac.in>
2026-06-20 16:54:07 +0530

    first commit

---
+++ Dummycode.java
@@ -0,0 +1,9 @@
+public class Dummycode {
+    public static void dummyMethod() {
+        System.out.println("This is a dummy method.")
+    }
+    public static void main(String[] args) {
+        System.out.println("This is a dummy code.");
+        dummyMethod();
+    }
+}
