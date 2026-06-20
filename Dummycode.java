b38d51dfd7361fc194d1e1c5fe61b0cecb856681
sssinha0 <b180043@nitsikkim.ac.in>
2026-06-20 18:43:52 +0530

    dummy commit to check jira connection KAN-1

--- Dummycode.java
+++ Dummycode.java
@@ -2,6 +2,7 @@ public class Dummycode {
     public static void dummyMethod() {
         System.out.println("This is a dummy method.")
     }
+    //dummy commit to check jira connected or not?
     public static void main(String[] args) {
         System.out.println("This is a dummy code.");
         dummyMethod();
