5738b4c65af31299054fa297d339fe396924b18e
sssinha0 <b180043@nitsikkim.ac.in>
2026-06-20 18:48:32 +0530

    KAN-1 fix: added missing semicolon

--- Dummycode.java
+++ Dummycode.java
@@ -2,7 +2,7 @@ public class Dummycode {
     public static void dummyMethod() {
         System.out.println("This is a dummy method.")
     }
-    //dummy commit to check jira connected or not?
+    //dummy commit to check jira connected or not
     public static void main(String[] args) {
         System.out.println("This is a dummy code.");
         dummyMethod();
