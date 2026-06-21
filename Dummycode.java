ea26fe437e835a61fbf39b9e11186737fd9cb109
sssinha0 <b180043@nitsikkim.ac.in>
2026-06-21 18:13:07 +0530

    AI Fix for KAN-2

--- Dummycode.java
+++ Dummycode.java
@@ -1,8 +1,12 @@
 public class Dummycode {
-    public static void dummyMethod() {
-        System.out.println("This is a dummy method.")
+    public static int sum(int a, int b) {
+        return a + b;
     }
-    //dummy commit to check jira connected or not
+
+    public static void dummyMethod() {
+        System.out.println("This is a dummy method.");
+    }
+
     public static void main(String[] args) {
         System.out.println("This is a dummy code.");
         dummyMethod();
